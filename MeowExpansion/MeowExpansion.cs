fb240e1d7d4f47187468f50fc84d0c07f361545f
NotZer0Two <lyonfan2010@gmail.com>
2024-08-28 12:07:47 +0200

    Aggiungere i file di progetto.

---
+++ MeowExpansion/MeowExpansion.cs
@@ -0,0 +1,23 @@
+﻿using PlaceholderAPI;
+using PlaceholderAPI.API.Abstract;
+using System.Reflection;
+
+namespace MeowExpansion
+{
+    public class MeowExpansion : PlaceholderExpansion
+    {
+        public override string Author { get; set; } = "NotZer0Two";
+        public override string Identifier { get; set; } = "meow";
+        public override string RequiredPlugin { get; set; } = "HintServiceMeow";
+
+        public override bool CanRegister()
+        {
+            bool original = base.CanRegister();
+
+            if(original)
+                PlaceholderAPIPlugin.HarmonyPatch.PatchAll(typeof(MeowExpansion).Assembly);
+
+            return original;
+        }
+    }
+}
---
+++ MeowExpansion/MeowHintDisplayPatch.cs
@@ -0,0 +1,48 @@
+﻿using Exiled.API.Features;
+using Exiled.API.Features.Pools;
+using HarmonyLib;
+using Hints;
+using HintServiceMeow.Core.Utilities;
+using PlaceholderAPI.API;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+using static HarmonyLib.AccessTools;
+
+namespace MeowExpansion
+{
+    [HarmonyPatch(typeof(HintServiceMeow.Core.Utilities.PlayerDisplay), "UpdateHint")]
+    public class MeowHintDisplayPatch
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
+
+            int index = newInstructions.FindLastIndex(instruction => instruction.opcode == OpCodes.Stloc_0);
+
+            newInstructions.InsertRange(index + 1, new CodeInstruction[]
+            {
+                //message
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldloc_0),
+
+                //this
+                new CodeInstruction(OpCodes.Ldarg_0),
+
+                //message = Display(string, Referencehub);
+                new CodeInstruction(OpCodes.Call, Method(typeof(MeowHintDisplayPatch), nameof(Display))),
+                new CodeInstruction(OpCodes.Stloc_0),
+            });
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Pool.Return(newInstructions);
+        }
+
+        public static string Display(string message, PlayerDisplay hub)
+        {
+            return PlaceholderAPI.API.PlaceholderAPI.SetPlaceholders(Player.Get(hub.ReferenceHub), message);
+        }
+    }
+}
